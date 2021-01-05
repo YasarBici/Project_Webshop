156e36cc81f0cdb098ded748854e8de394bd5bae
yasar <2105076@talnet.nl>
2021-01-05 16:50:38 +0100

    Webshop

---
+++ webshop/index.php
@@ -0,0 +1,18 @@
+<?php
+
+session_start();
+
+include_once("DBConfig.php");
+include_once("header.html");
+
+if (isset($_GET["page"])) {
+    $page = $_GET["page"];
+} else {
+    $page = "inloggen";
+}
+
+if ($page) {
+    include("pages/" . $page . ".php");
+}
+
+?>
